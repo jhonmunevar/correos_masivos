e79cc68f10a92ffee0b80c367a8e343a83fc5c76
Humberto Zuluaga <hzuluaga@gmail.com>
2019-06-05 17:59:37 -0500

    nueva lista de destinatarios

--- resources/views/emails/invitacion_acreditacion_victimas.blade.php
+++ resources/views/emails/invitacion_acreditacion_victimas.blade.php
@@ -13,17 +13,19 @@ Reciba un cordial saludo.
 
  
 
-En atención a su condición de víctima reconocida en el proceso de Justicia y Paz por el delito de secuestro cometido por la extinta guerrilla de las FARC-EP, la Sala de Reconocimiento, de Verdad, de Responsabilidad y de Determinación de Hechos y Conductas de la Jurisdicción Especial para la Paz (JEP) le extiende una invitación para acreditarse como víctima en el Caso No. 001 denominado “Retención ilegal de personas por parte de las FARC-EP”.
+En atención a su condición de víctima reconocida en el proceso de Justicia y Paz por el delito de secuestro cometido por la extinta guerrilla de las FARC-EP, la Sala de Reconocimiento, de Verdad, de Responsabilidad y de Determinación de Hechos y Conductas de la Jurisdicción Especial para la Paz (JEP) le extiende una invitación para acreditarse como víctima en el Caso No. 01 denominado “Retención ilegal de personas por parte de las FARC-EP”.
 
  
 
-La acreditación le otorgará la calidad de interviniente especial en el proceso ante la JEP, y con ello, su derecho a participar en todas las instancias del Caso 001 que se adelanta frente a los ex miembros de las FARC-EP. 
+La acreditación le otorgará la calidad de interviniente especial en el proceso ante la JEP, y con ello, obtendrá su derecho a participar en todas las instancias del Caso 01 que se adelanta frente a los ex miembros de las FARC-EP.  
 
-#Para adquirir este derecho, sólo requiere manifestar su voluntad de acreditarse respondiendo a este correo adjuntando el formato anexo diligenciado junto los documentos de soporte.
+#Para adquirir este derecho, sólo requiere manifestar su voluntad de acreditarse respondiendo a este correo, adjuntando el formato anexo diligenciado y los documentos que soportan su solicitud.
+
+
+La acreditación es el primer paso para materializar el principio de centralidad de las víctimas que orienta todas las actuaciones judiciales de la Jurisdicción Especial para la Paz. Valoramos la confianza que deposita en nosotros en caso de decidir acreditar su condición de víctima en el Caso 01
 
  
-
-Antes de explicar en mayor detalle qué derechos se desprenden de esta acreditación y cómo actuar ante la JEP, permítanos exponer una información básica sobre el Caso al que se le invita a participar.
+Antes de explicar en mayor detalle qué derechos se desprenden de esta acreditación y cómo puede actuar ante la JEP, permítanos exponer una información básica sobre el Caso al que se le invita a participar.
 
  
 
@@ -31,30 +33,30 @@ Antes de explicar en mayor detalle qué derechos se desprenden de esta acreditac
 
  
 
-La Sala de Reconocimiento de la JEP avocó conocimiento del Caso No. 001 el 4 de julio de 2018 a partir del Informe No. 2 presentado por la Fiscalía General de la Nación, denominado “Retención ilegal de personas por parte de las FARC-EP”. El 13 de julio de 2018, la Sala notificó en diligencia pública el inicio del Caso a 31 miembros del Estado Mayor Central de la antigua guerrilla de las FARC-EP y decretó abierta la etapa de “reconocimiento de verdad, de responsabilidad y de determinación de los hechos y las conductas”[1]. Actualmente, la Sala se encuentra adelantando las diligencias de versiones voluntarias de los ex miembros del Estado Mayor de las FARC, siendo este el primer momento de aporte a la verdad. 
+La Sala de Reconocimiento de la JEP avocó conocimiento del Caso No. 01 el 4 de julio de 2018 a partir del Informe No. 2 presentado por la Fiscalía General de la Nación, denominado “Retención ilegal de personas por parte de las FARC-EP”. El 13 de julio de 2018, la Sala notificó en diligencia pública el inicio del Caso a 31 ex miembros del Estado Mayor Central de la antigua guerrilla de las FARC-EP y decretó abierta la etapa de “reconocimiento de verdad, de responsabilidad y de determinación de los hechos y las conductas”[1] . Actualmente, la Sala se encuentra adelantando las diligencias de versiones voluntarias de ex miembros del Estado Mayor y otros ex combatientes de las FARC, siendo este el primer momento de aporte a la verdad.
 
  
 
-De acuerdo con las normas que rigen la Jurisdicción Especial para la Paz, la Sala debe desarrollar un proceso de construcción dialógica de la verdad, en donde las víctimas están en el centro. Esto significa que la Sala debe escuchar a las víctimas y a los comparecientes y debe contrastar las diferentes versiones de lo sucedido y los daños ocasionados, a la luz de otras fuentes de información para determinar la verdad. En el marco de este proceso, los comparecientes - ex miembros de las FARC-EP implicados en conductas de retenciones ilegales- deben reconocer su responsabilidad frente a los crímenes cometidos y aportar a la verdad, garantizando los derechos de las víctimas a la verdad, a la reparación y a la no repetición. De no hacerlo, se aplicarán consecuencias relacionadas con la pérdida de beneficios del Sistema Integral de Verdad, Justicia, Reparación y No Repetición de acuerdo a lo establecido en la ley y en el Acuerdo Final de Paz[2].
+De acuerdo con las normas que rigen la JEP, la Sala debe desarrollar un proceso de construcción dialógica de la verdad, en el cual las víctimas están en el centro. Esto significa que la Sala debe escuchar a las víctimas y a los comparecientes, y debe contrastar las diferentes versiones de lo sucedido y los daños ocasionados a la luz de otras fuentes de información para esclarecer la verdad sobre lo ocurrido. En el marco de este proceso, los comparecientes –ex miembros de las FARC-EP implicados en conductas que incluyen secuestro de políticos o con finalidades de canje; extorsivo; de militares y policías y secuestro que deriva en desaparición forzada– deben reconocer su responsabilidad frente a los crímenes cometidos y aportar al esclarecimiento de la verdad, garantizando los derechos de las víctimas a la verdad, a la reparación y a la no repetición. De no hacerlo, se aplicarán consecuencias relacionadas con la pérdida de beneficios del Sistema Integral de Verdad, Justicia, Reparación y No Repetición de acuerdo con el marco normativo que desarrolla el Acuerdo Final de Paz[2]
 
  
 
 #Sobre los derechos de participación derivados de la acreditación como víctima en el Caso No. 001
 
 
-Las víctimas acreditadas en el marco de los procedimientos ante la JEP adquieren los derechos de un interviniente especial, según los estándares nacionales e internacionales sobre garantías procesales, sustanciales, probatorias, acceso a un recurso judicial efectivo y demás derechos aplicables. Los artículos 14, 15 y 17 del Proyecto de Ley Estatutaria de la JEP consagran los derechos de las víctimas acreditadas ante la JEP. Entre otros, las víctimas pueden:
+Las víctimas acreditadas en el marco de los procedimientos ante la JEP adquieren los derechos de un interviniente especial, según los estándares nacionales e internacionales sobre garantías procesales, sustanciales, probatorias, acceso a un recurso judicial efectivo y demás derechos aplicables. Los artículos 14, 15 y 17 del Proyecto de Ley Estatutaria de la JEP consagran los derechos de las víctimas acreditadas ante la JEP. Entre otras facultades, las víctimas pueden:
 
  
 
-1.     Aportar pruebas e interponer recursos contra las sentencias que se profieran.
+1.     Aportar pruebas e interponer recursos contra las sentencias que se profieran;
 
-2.     Recibir asesoría, orientación y representación judicial a través del Sistema Autónomo de Asesoría y Defensa (SAAD).
+2.     Recibir asesoría, orientación y representación judicial a través del Sistema Autónomo de Asesoría y Defensa (SAAD);
 
-3.     Contar con acompañamiento psicosocial y jurídico en los procedimientos.
+3.     Contar con acompañamiento psicosocial y jurídico en los procedimientos;
 
-4.     Ser informadas del avance de la investigación y del proceso, y de cuándo se llevarán a cabo las distintas audiencias del proceso, e intervenir en ellas.
+4.     Ser informadas del avance de la investigación y del proceso, y de cuándo se llevarán a cabo las distintas audiencias del proceso, e intervenir en ellas, y
 
-5.     Solicitar medidas de protección en el caso que sus derechos fundamentales se encontrasen amenazados por su participación en el proceso ante la JEP.
+5.     Solicitar medidas de protección en el caso que sus derechos fundamentales se encontrasen amenazados por su participación en el proceso ante la JEP,
 
  
 
@@ -62,17 +64,20 @@ Concretamente, ante la Sala de Reconocimiento de Verdad y Responsabilidad, que e
 
  
 
-1.    Aportar pruebas y, con posterioridad a la recepción de versiones voluntarias, presentar observaciones a estas y solicitar y recibir copia del expediente.
+1.    Aportar pruebas y, con posterioridad a la recepción de versiones voluntarias, presentar observaciones a estas y solicitar y recibir copia del expediente;
 
-2.    Asistir a la audiencia pública de reconocimiento, de haberla, y dentro de los 15 días hábiles posteriores a la audiencia, presentar observaciones finales escritas sobre todos los aspectos concernientes a la Resolución de Conclusiones.
+2.    Asistir a la audiencia pública de reconocimiento, de haberla, y dentro de los 15 días hábiles posteriores a la audiencia, presentar observaciones finales escritas sobre todos los aspectos concernientes a la Resolución de Conclusiones;
 
-3.    Presentar observaciones en relación con los proyectos restaurativos propuestos por la persona compareciente.
+3.    Presentar observaciones en relación con los proyectos restaurativos propuestos por la persona compareciente; 
 
-4.    Las víctimas de violencia basada en género, incluyendo aquellas de violencia sexual, tienen derecho a no ser confrontadas con su agresor.
+4.    Las víctimas de violencia basada en género, incluyendo aquellas de violencia sexual, tienen derecho a no ser confrontadas con su agresor;
 
-5.    Interponer recursos en contra de la decisión sobre selección de casos.
+5.    Interponer recursos en contra de la decisión sobre selección de casos, y
 
-6.    Formular preguntas frente a sus hechos victimizantes.
+6.    Formular preguntas específicas frente a las circunstancias relacionadas con la ocurrencia de sus hechos victimizantes.
+
+
+Con respecto al derecho a la reparación, debe tenerse en cuenta que no corresponde a la Jurisdicción imponer sanciones indemnizatorias de perjuicios. Las indemnizaciones estarán a cargo del Estado a través del programa masivo de reparaciones, de acuerdo al derecho de las víctimas a la reparación integral y en concordancia con la Ley 1448 de 2011. Sin perjuicio de lo anterior, en virtud del artículo 18 transitorio del A.L. 1 de 2017 “los comparecientes conservan la obligación general de reparar a las víctimas a través de medidas no indemnizatorias, las cuales pueden consistir en obligaciones de reparación impuestas por la JEP en cumplimiento de lo establecido en el SIVJRNR”, y que hacen parte del régimen de condicionalidad. (Ver, Corte Constitucional, Sentencia C-080 de 2018, párr. 4.1.7.4.). En ese contexto, la JEP podrá imponer sanciones que incluyan la obligación de los comparecientes de realizar trabajos, obras y actividades con contenido reparador. En todo caso, los bienes y activos patrimoniales de las extintas FARC-EP (reportados y no reportados en el inventario entregado al Gobierno Nacional) servirán para financiar la reparación material de las víctimas que se podrán otorgar a través de distintos mecanismos[4].
 
  
 
@@ -95,18 +100,12 @@ Si los miembros de su familia o allegados padecieron un daño por los hechos vic
 #¿Cómo puede ser representado en el proceso ante la JEP?
 
  
-
 La ley prevé la posibilidad de participar en el proceso por sí mismo, por medio abogado de confianza, por abogado designado por su organización de víctimas (si existe), o por medio de representante común designado por el Sistema Autónomo de Asesoría y Defensa administrado por la Secretaría Ejecutiva de la JEP, siempre que tenga su consentimiento.
 
  
-
 En caso de tomar la decisión de ser representado por apoderado de confianza, resulta necesario allegar un poder especial para actuar ante la JEP. En caso de desear ser representado por el abogado común asignado por la JEP, favor indicarlo en el formato de solicitud de acreditación que se adjunta a este correo.
 
- 
 
-Valoramos la confianza que deposita en nosotros al decidir acudir a esta entidad. La acreditación de las víctimas busca materializar el principio de centralidad de las víctimas que orienta todas las actuaciones de la Jurisdicción Especial para la Paz. 
-
- 
 
 Quedamos a su disposición para atender cualquier duda o consulta, y agradecemos su participación en el Caso 001.
 
@@ -124,15 +123,15 @@ Cordialmente,
 
 
 
-
-
 <h5> 
-[1] Para mayor información sobre el Caso 001, ver: JEP, Sala de Reconocimiento, Auto No. 002 del 4 de julio del 2018, por el cual se avoca conocimiento del Caso 001 sobre retenciones ilegales por parte de las FARC-EP. Disponible en: https://www.jep.gov.co/Relatoria/Sala%20de%20Reconocimiento%20de%20Verdad,%20de%20Responsabilidad%20y%20de%20Determinaci%C3%B3n%20de%20los%20Hechos%20y%20Conductas/Autos/Auto_SRVR-002%20de%2004%20de%20julio%20de%202018.pdf.</h5>
+[1] <a href="https://www.jep.gov.co/Relatoria/Sala%20de%20Reconocimiento%20de%20Verdad,%20de%20Responsabilidad%20y%20de%20Determinaci%C3%B3n%20de%20los%20Hechos%20y%20Conductas/Autos/Auto_SRVR-002%20de%2004%20de%20julio%20de%202018.pdf">Para mayor información sobre el Caso 001, ver: JEP, Sala de Reconocimiento, Auto No. 002 del 4 de julio del 2018, por el cual se avoca conocimiento del Caso 001 sobre retenciones ilegales por parte de las FARC-EP.</a></h5>
 
-<h5> [2] Para mayor información sobre el funcionamiento de la JEP, puede consultar las preguntas frecuentes disponibles en la página web: https://www.jep.gov.co/Paginas/Servicio-al-Ciudadano/preguntas.aspx.</h5>
-
- 
+<h5> [2] Para mayor información sobre el funcionamiento de la JEP, puede consultar las preguntas frecuentes disponibles en <a href="https://www.jep.gov.co/Paginas/Servicio-al-Ciudadano/preguntas.aspx">la página web de la JEP.</a></h5>
 
 <h5> [3] Artículos 13.5 y 27.D de la Ley 1922 de 2018.</h5>
+
+<h5> [4] Ver Decreto Ley 903 de 2017 y Sentencia C-071 de 2018.</h5>
+
+
 
 @endcomponent
