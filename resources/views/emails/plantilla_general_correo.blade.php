1a3e9a8c4c3fe4e3a5fabd376b42f5c55b40f8e2
Humberto Zuluaga <hzuluaga@gmail.com>
2019-11-27 15:11:37 -0500

    otros fixes

--- resources/views/emails/plantilla_general_correo.blade.php
+++ resources/views/emails/plantilla_general_correo.blade.php
@@ -5,16 +5,28 @@ Reciba un cordial saludo.
 <BLOCKQUOTE>
  
 De acuerdo con su solicitud, la Sala de Reconocimiento de Verdad, Responsabilidad y Determinación de Hechos y Conductas de la JEP lo acreditó como víctima en el caso 001, denominado “Retención Ilegal de Personas por parte de las FARC – EP”. En el marco de dicha solicitud, usted manifestó requerir de un representante judicial del Sistema Autónomo de Asesoría y Defensa – SAAD.
- 
+
+</BLOCKQUOTE>
+
+<BLOCKQUOTE> 
 Teniendo en cuenta lo anterior, adjunto a esta comunicación encontrará un formato de poder mediante el cual usted otorga la facultad un abogado del SAAD para representarlo ante la JEP a en el marco del Caso 001.
+
+</BLOCKQUOTE>
  
+<BLOCKQUOTE>
 De tal forma que, si usted se encuentra de acuerdo con dicha representación, le solicitamos lo diligencie únicamente con sus datos (nombre completo, tipo y número de documento), lo presente en una notaría para su autenticación, y luego allegue el documento original por medio de correo postal a la JEP: carrera 7 # 63 – 44 - Bogotá, dirigido al “Sistema Autónomo de Asesoría y Defensa – Representación de Víctimas - SAAD – Víctimas”.
  
+</BLOCKQUOTE>
+<BLOCKQUOTE> 
 Una vez le sea asignado un abogado, éste se comunicará con usted.
+
+#Si usted ya envió el poder respectivo, le rogamos hacer caso omiso a este mensaje.
  
 </BLOCKQUOTE>
 
-#SI USTED YA ENVÍO EL PODER RESPECTIVO, LE ROGAMOS HACER CASO OMISO A ESTE MENSAJE.
+
+
+
  
 #Sistema Autónomo de Asesoría y Defensa – Representación a víctimas
  
