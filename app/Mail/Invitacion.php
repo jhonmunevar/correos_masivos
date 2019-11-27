00f819afbc53148e530cd37e84349e2bd95173fd
Humberto Zuluaga <hzuluaga@gmail.com>
2019-11-27 14:36:55 -0500

    envio de poder

--- app/Mail/Invitacion.php
+++ app/Mail/Invitacion.php
@@ -37,25 +37,25 @@ class Invitacion extends Mailable implements ShouldQueue
         return $this->markdown('emails.plantilla_general_correo')
               //  ->from('victimas.caso001@jep.gov.co')
                // ->replyTo('claudia.erazo@jep.gov.co')
-                ->subject('Caso No. 01 – Auto de apertura de trámite de traslado para la presentación de observaciones a Versiones Voluntarias')
+                ->subject('Sistema Autónomo de Asesoría y Defensa – Representación a víctima')
                 // ->with([
                 //     'name' => $this->person->nombre1.' '.$this->person->apellido1 ,
                 //     'gender' => $this->person->sexo
 
                 // ])
-                ->attach(storage_path('app/sources/Oficio_No_9227_Caso_01.pdf'), 
-                    ['as'=>'Oficio No. 9227 - Caso No. 01.pdf',
-                    'mime' => 'application/pdf'
-                    // 'mime' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                ->attach(storage_path('app/sources/Poder_para_representación_judicial_v1.docx'), 
+                    ['as'=>'Poder para representación judicial.docx',
+                   // 'mime' => 'application/pdf'
+                     'mime' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
                     
                 ])
 
-                ->attach(storage_path('app/sources/Auto_22_de_noviembre.pdf'), 
-                    ['as'=>'Auto 22 de noviembre.pdf',
-                    'mime' => 'application/pdf'
-                    // 'mime' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                // ->attach(storage_path('app/sources/Auto_22_de_noviembre.pdf'), 
+                //     ['as'=>'Auto 22 de noviembre.pdf',
+                //     'mime' => 'application/pdf'
+                //     // 'mime' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
       
-                ])
+                // ])
 
 
                 ;
--- resources/views/emails/plantilla_general_correo.blade.php
+++ resources/views/emails/plantilla_general_correo.blade.php
@@ -1,18 +1,22 @@
 @component('mail::message')
 
+Reciba un cordial saludo.
  
-Reciba cordial saludo.
+<BLOCKQUOTE>
  
-<BLOCKQUOTE>En atención a su condición de víctima acreditada en el Caso No. 01 denominado “Retención ilegal de personas por parte de las FARC-EP”, la Sala de Reconocimiento de Verdad, de Responsabilidad y de Determinación de los Hechos y Conductas de la Jurisdicción Especial para la Paz, le comunica a continuación de la Apertura del Trámite de traslado para la Presentación de Observaciones a las Versiones Voluntarias en el Caso No. 01.</BLOCKQUOTE>
-
-<BLOCKQUOTE>El traslado le permitirá conocer el contenido de las Versiones Voluntarias practicadas en el marco del Caso No. 01 a los ex miembros de las extintas FARC-Ep, y posteriormente, contribuir a la verdad integral haciendo observaciones a este contenido.</BLOCKQUOTE>
-
-
-<h5> <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=wdIp156Y5US3xRgskUrGB-P057QmPQNGsfjircIG_s9UM0dUNE8yWTJETDVOSlZIN0xaR0RBRkw5NC4u">Para informar a la Sala sobre la modalidad de traslado de su preferencia, por favor ingrese al siguiente link</a></h5>
-
-
+De acuerdo con su solicitud, la Sala de Reconocimiento de Verdad, Responsabilidad y Determinación de Hechos y Conductas de la JEP lo acreditó como víctima en el caso 001, denominado “Retención Ilegal de Personas por parte de las FARC – EP”. En el marco de dicha solicitud, usted manifestó requerir de un representante judicial del Sistema Autónomo de Asesoría y Defensa – SAAD.
  
-Agradecemos su participación en el Caso No. 01 y la confianza depositada en esta Jurisdicción.
+Teniendo en cuenta lo anterior, adjunto a esta comunicación encontrará un formato de poder mediante el cual usted otorga la facultad un abogado del SAAD para representarlo ante la JEP a en el marco del Caso 001.
+ 
+De tal forma que, si usted se encuentra de acuerdo con dicha representación, le solicitamos lo diligencie únicamente con sus datos (nombre completo, tipo y número de documento), lo presente en una notaría para su autenticación, y luego allegue el documento original por medio de correo postal a la JEP: carrera 7 # 63 – 44 - Bogotá, dirigido al “Sistema Autónomo de Asesoría y Defensa – Representación de Víctimas - SAAD – Víctimas”.
+ 
+Una vez le sea asignado un abogado, éste se comunicará con usted.
+ 
+</BLOCKQUOTE>
+
+#SI USTED YA ENVÍO EL PODER RESPECTIVO, LE ROGAMOS HACER CASO OMISO A ESTE MENSAJE.
+ 
+#Sistema Autónomo de Asesoría y Defensa – Representación a víctimas
  
 
 Cordialmente,
