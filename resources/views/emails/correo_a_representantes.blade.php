1bd36ddd7592014e78ab09b896794c620fd155ce
Humberto Zuluaga <hzuluaga@gmail.com>
2019-12-15 21:28:49 -0500

    cambio para victimas

---
+++ resources/views/emails/correo_a_representantes.blade.php
@@ -0,0 +1,53 @@
+ 
+@component('mail::message')
+ 
+Estimado señor(a),
+
+Reciba un cordial saludo por parte de la Jurisdicción Especial para la Paz (JEP).
+ 
+<BLOCKQUOTE>
+Como le fue comunicado mediante Auto de 22 de noviembre de 2019 enviado por este medio, en su calidad de representante judicial de una de las víctimas acreditadas en el Caso 01 de la Sala de Reconocimiento de Verdad, Responsabilidad y Determinación de los Hechos y Conductas denominado “Retenciones ilegales por parte de las FARC-EP”, usted podrá conocer el contenido de las versiones voluntarias rendidas por los ex combatientes de la antigua guerrilla de las FARC-Ep en el marco del Caso y presentar observaciones a las mismas.
+</BLOCKQUOTE>
+
+<BLOCKQUOTE>
+De conformidad con nuestro registro, la víctima que usted representa ha manifestado interés en acceder a dicha información mediante el traslado de las versiones voluntarias. Para que usted pueda hacerlo, deberá:
+
+<ol>
+	<li>Acceder al siguiente enlace: <a href = "http://abogados.jep.gov.co/traslado">abogados.jep.gov.co/traslado</a>.</li>
+	<li>Ingresar su número de documento de identidad.</li>
+	<li>Ingresar su correo electrónico como representante judicial indicado al momento de la solicitud de acreditación por parte de la vícitma.</li>
+	<li>En la plataforma podrá filtrar por el número de identificación de la víctima que representa por la que va a presentar observaciones.</li>
+</ol>
+ 
+</BLOCKQUOTE>
+
+<BLOCKQUOTE>
+Una vez ingrese a la plataforma, usted podrá conocer el contenido de las versiones voluntarias de las estructuras de las extintas FARC – Ep de acuerdo al lugar de ocurrencia de los hechos que la víctima que usted representa indicara en la solicitud de acreditación, y podrá diligenciar el Formulario en línea dispuesto para la presentación de observaciones por parte de representantes judiciales. 
+</BLOCKQUOTE>
+
+<BLOCKQUOTE>
+Por favor recuerde que, a partir del momento en que usted accede al enlace, cuenta con 20 días hábiles para presentar sus observaciones.  
+</BLOCKQUOTE>
+
+<BLOCKQUOTE>
+Si tiene alguna duda o consulta, usted puede comunicarse al correo <a href = "mailto:victimas.caso001@jep.gov.co">victimas.caso001@jep.gov.co</a>.  
+</BLOCKQUOTE>
+
+<br> 
+<b>Agradecemos la confianza depositada en la Jurisdicción Especial para la Paz.</b>
+ 
+<br>
+Cordialmente,
+
+
+
+#Jurisdicción Especial para la Paz
+
+![JEP][logo]
+
+[logo]: {{asset('jep_logo.png')}}
+
+
+
+
+@endcomponent
