6908c4e794f9f1415ee0e9b2e970f6df71c60f3f
Humberto Zuluaga <hzuluaga@gmail.com>
2019-12-16 21:01:18 -0500

    no message

---
+++ resources/views/emails/correo_victimias.blade.php
@@ -0,0 +1,53 @@
+ 
+@component('mail::message')
+ 
+Estimado señor(a),
+
+Reciba un cordial saludo por parte de la Jurisdicción Especial para la Paz (JEP).
+ 
+<BLOCKQUOTE>
+Como le fue comunicado mediante Auto del 22 de noviembre de 2019 enviado por este medio, como víctima acreditada en el Caso 01 de la Sala de Reconocimiento de Verdad, Responsabilidad y Determinación de los Hechos y Conductas denominado “Retenciones ilegales por parte de las FARC-EP”, usted tiene el derecho a conocer el contenido de las versiones voluntarias rendidas por los ex combatientes de la antigua guerrilla de las FARC-Ep en el marco del Caso y presentar sus observaciones a las mismas.
+</BLOCKQUOTE>
+
+<BLOCKQUOTE>
+De conformidad con nuestro registro, usted ha manifestado interés en acceder a dicha información mediante el traslado de las versiones voluntarias. Para hacerlo, usted deberá:
+
+<ol>
+	<li>Acceder al siguiente enlace: <a href = "http://abogados.jep.gov.co/traslado">abogados.jep.gov.co/traslado</a>.</li>
+	<li>Ingresar su número de documento de identidad.</li>
+	<li>Ingresar el correo electrónico indicado al momento de realizar su solicitud de acreditación como víctima en el Caso.</li>
+	
+</ol>
+ 
+</BLOCKQUOTE>
+
+<BLOCKQUOTE>
+Una vez ingrese a la plataforma, usted podrá conocer el contenido de las versiones voluntarias de las estructuras de las extintas FARC – Ep de acuerdo al lugar de ocurrencia de los hechos que usted indicara en su solicitud de acreditación, y podrá diligenciar el Formulario en línea dispuesto para la presentación de sus observaciones.
+</BLOCKQUOTE>
+
+<BLOCKQUOTE>
+Por favor recuerde que, a partir del momento en que usted accede al enlace, cuenta con 20 días hábiles para presentar sus observaciones.  
+</BLOCKQUOTE>
+
+<BLOCKQUOTE>
+Si tiene alguna duda o consulta, usted puede comunicarse al correo <a href = "mailto:victimas.caso001@jep.gov.co">victimas.caso001@jep.gov.co</a>.  
+</BLOCKQUOTE>
+
+<br> 
+<b>Agradecemos la confianza depositada en la Jurisdicción Especial para la Paz.</b>
+ 
+<br>
+Cordialmente,
+
+
+
+#Jurisdicción Especial para la Paz
+
+![JEP][logo]
+
+[logo]: {{asset('jep_logo.png')}}
+
+
+
+
+@endcomponent
